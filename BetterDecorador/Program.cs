dbffb3014e15c76fd36f29f5339ec82d314cbd6b
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-10 13:46:49 +0000

    Added BetterDecorador project

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
