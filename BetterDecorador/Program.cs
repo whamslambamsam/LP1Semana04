dab2c1b2cca751b89421b44476456d95db9efbb2
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-10 15:55:44 +0000

    Completed BetterDecorador project

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -12,7 +12,11 @@ namespace BetterDecorador
 
         static void Main(string[] args)
         {
+            string line = args[0];
+            char character = Convert.ToChar(args[1]);
+            int num = Convert.ToInt32(args[2]);
 
+            Console.WriteLine(Decor(line, character, num));
         }
     }
 }
